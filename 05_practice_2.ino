c9de33b755ab6274f56b45d8a6680503fc15bc26
kmu-jaewon <132831980+wooju050506@users.noreply.github.com>
2024-10-01 02:13:40 +0900

    Add files via upload

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,22 +1,24 @@
 int ledPin = 7; 
+
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT); 
 }
 
 void loop() {
-
-  digitalWrite(ledPin, HIGH); 
+  digitalWrite(ledPin, LOW);
   delay(1000); 
 
   for (int i = 0; i < 5; i++) {
-    digitalWrite(ledPin, LOW);  
-    delay(100); 
-    digitalWrite(ledPin, HIGH); 
+    digitalWrite(ledPin, LOW); 
+    delay(100);
+    digitalWrite(ledPin, HIGH);
     delay(100); 
   }
 
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, HIGH);
+
 
   while (1) {
+   
   }
 }
